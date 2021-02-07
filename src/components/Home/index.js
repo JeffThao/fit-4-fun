cd8b9e935750134b08e9e31f5a7c0e037d6d9d76
Jeff T <jeff.thao92@gmail.com>
2021-02-07 16:28:58 -0600

    adding all changes. also updated readme.

---
+++ src/components/Home/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import NavBar from "../NavBar";
+
+function Home () {
+return (
+    <div>
+        <NavBar />
+        <Container >
+            <Row justify-content-center>
+                <Col>
+                <div>
+                    
+                </div>
+                </Col>
+            </Row>
+        </Container>
+
+    </div>
+
+)
+
+}
+
+export default Home;
