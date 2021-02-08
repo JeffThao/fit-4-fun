d9fd7203a4bc34f6938e2bdd17bb89a7ad318ace
Jeff T <jeff.thao92@gmail.com>
2021-02-07 18:44:14 -0600

    Took out home link in navbar, added log in and sign up.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,26 +2,23 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import NavBar from "../NavBar";
 
-function Home () {
-return (
+function Home() {
+  return (
     <div>
-        <NavBar />
-        <Container >
-            <Row justify-content-center>
-                <Col>
-                <div>
-                    <div>
-                        
-                    </div>
-                </div>
-                </Col>
-            </Row>
-        </Container>
-
+      <NavBar />
+      <Container>
+        <Row justify-content-center>
+          <Col>
+            <div className="">
+              <div className="my-5 mx-5">
+                <h2>Start tracking your nutritional goals.</h2>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-
-)
-
+  );
 }
 
 export default Home;
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,14 +5,11 @@ function NavBar() {
 
     return (
         <Navbar bg="light" expand="lg">
-           <Navbar.Brand href="#home">Fitness For Fun</Navbar.Brand>
-           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-           <Navbar.Collapse id="basic-navbar-nav">
-             <Nav className="ml-auto">
-               <Nav.Link href="#home">Home</Nav.Link>
-               <Nav.Link href="#home">Login/Sign-up</Nav.Link>
+           <Navbar.Brand className="ml-5" href="#home">Fitness For Fun</Navbar.Brand>
+             <Nav className="ml-auto mr-5">
+               <Nav.Link href="#home">Log In</Nav.Link>
+               <Nav.Link href="#home">Sign Up</Nav.Link>
              </Nav>
-          </Navbar.Collapse>
          </Navbar>
     );
 }
